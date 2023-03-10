0f23e5277976f62e4f20fdae7e99dd726e7fd75e
wtsiwon <siwon4727@gmail.com>
2023-03-10 11:18:23 +0900

    2번적 공격

--- KineungContestPractice1/Assets/Scripts/Enemy/Enemy2.cs
+++ KineungContestPractice1/Assets/Scripts/Enemy/Enemy2.cs
@@ -6,6 +6,21 @@ public class Enemy2 : Enemy
 {
     protected override void Attack()
     {
+        CircleDelayShot(12);
+    }
 
+    private void CircleDelayShot(int count)
+    {
+        StartCoroutine(nameof(IShot));
+    }
+
+    private IEnumerator IShot(int count)
+    {
+        for (int i = 0; i < 360; i+= 360 / count)
+        {
+            Bullet bullet1 = Instantiate(bullet);
+            bullet1.SetBullet(transform.position, new Vector3(0, 0, i), bulletSpd, dmg, true);
+        }
+        yield break;
     }
 }
